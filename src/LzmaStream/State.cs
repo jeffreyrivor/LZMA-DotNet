aabb030dbd75e747510806233a03aeca97f5b48f
Jeffrey Rivor <jeffreyrivor@gmail.com>
2021-01-18 18:27:12 -0800

    Extract State struct and fix block copy in decoder

---
+++ src/LzmaStream/State.cs
@@ -0,0 +1,36 @@
+﻿namespace Lzma
+{
+    struct State
+    {
+        public uint Index { get; private set; }
+
+        public bool IsCharState() => Index < 7;
+
+        public void UpdateChar()
+        {
+            if (Index < 4)
+            {
+                Index = 0;
+            }
+            else
+            {
+                Index -= Index < 10 ? 3u : 6u;
+            }
+        }
+
+        public void UpdateMatch()
+        {
+            Index = Index < 7 ? 7u : 10u;
+        }
+
+        public void UpdateRep()
+        {
+            Index = Index < 7 ? 8u : 11u;
+        }
+
+        public void UpdateShortRep()
+        {
+            Index = Index < 7 ? 9u : 11u;
+        }
+    }
+}
--- src/LzmaStream/StreamDecoder.cs
+++ src/LzmaStream/StreamDecoder.cs
@@ -265,13 +265,14 @@ namespace Lzma
         {
             var outputCount = 0;
 
-            while (outputCount < count && offset + outputCount < buffer.Length && CopyBlockLen-- > 0)
+            while (outputCount < count && offset + outputCount < buffer.Length && CopyBlockLen > 0)
             {
                 OutBlock[OutBlockPos++] = buffer[offset + outputCount++] = OutBlock[CopyBlockPos++];
                 CopyBlockPos %= BlockSize;
                 OutBlockPos %= BlockSize;
 
                 NumDecoded++;
+                CopyBlockLen--;
             }
 
             return outputCount;
@@ -281,40 +282,6 @@ namespace Lzma
         {
             len -= kMatchMinLen;
             return len < kNumLenToPosStates ? len : kNumLenToPosStates - 1;
-        }
-    }
-
-    struct State
-    {
-        public uint Index { get; private set; }
-
-        public bool IsCharState() => Index < 7;
-
-        public void UpdateChar()
-        {
-            if (Index < 4)
-            {
-                Index = 0;
-            }
-            else
-            {
-                Index -= Index < 10 ? 3u : 6u;
-            }
-        }
-
-        public void UpdateMatch()
-        {
-            Index = Index < 7 ? 7u : 10u;
-        }
-
-        public void UpdateRep()
-        {
-            Index = Index < 7 ? 8u : 11u;
-        }
-
-        public void UpdateShortRep()
-        {
-            Index = Index < 7 ? 9u : 11u;
         }
     }
 }
